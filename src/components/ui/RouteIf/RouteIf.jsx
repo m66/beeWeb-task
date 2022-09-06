14ac60e9a6218956eb95925017c7d3dcbbc76841
Karen Manasyan <kar.manasyan98@gmail.com>
2022-09-06 12:30:49 -0700

    add main spin/logo

--- src/components/ui/RouteIf/RouteIf.jsx
+++ src/components/ui/RouteIf/RouteIf.jsx
@@ -1,9 +1,15 @@
+import { Space, Spin } from "antd";
+
 const RouteIf = ({ elem, showWhen }) => {
   if (showWhen) {
     return elem;
   }
 
-  return null;
+  return (
+    <Space size="middle">
+      <Spin size="large" />
+    </Space>
+  );
 };
 
 export default RouteIf;
